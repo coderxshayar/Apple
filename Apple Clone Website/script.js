56b5bf30788940696db4740dd172b3f58201e92f
coderxshayar <kumarsinhaabhijit@gmail.com>
2023-12-28 20:11:57 +0530

    first commit

---
+++ Apple Clone Website/script.js
@@ -0,0 +1,5 @@
+let navlist = document.getElementsById('nav_lst')
+let openMenu=document.getElementsById('hemburger')
+openMenu.addEventListener('click',()=>{
+    navlist.style.opacity="100%";
+});
